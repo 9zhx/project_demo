e854a5155b78ae1cbf7d1749512247259860518c
monkey <15274632+zhhaixuan@user.noreply.gitee.com>
2025-12-04 16:01:08 +0800

    添加文件

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	cout<<"hello world!"<<endl; 
+	int sum=100;
+	cout<<"sum/10"<<sum/10<<endl;
+	return 0;
+}
