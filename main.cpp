ca2a088c852339717ca5b640a6b03cc2da9a82d3
9zhx <463176570@qq.com>
2025-12-05 21:11:18 +0800

    Change output message to 'hello world!v2'
    Updated the output message to 'hello world!v2'.

--- main.cpp
+++ main.cpp
@@ -4,11 +4,9 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-<<<<<<< HEAD
+
 	cout<<"hello world!v2"<<endl; 
-=======
-	cout<<"hello world! v1"<<endl; 
->>>>>>> 4013f8cc03537b16073331da37c2233658391d88
+
 	int sum=100;
 	cout<<"sum/10"<<sum/10<<endl;
 	return 0;
